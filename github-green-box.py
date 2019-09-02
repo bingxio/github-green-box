2451c38ca440c2ed3d98c49aface1ec7dd2cb295
Turaiiao <3106740988@qq.com>
2019-09-02 18:32:01 +0800

    First commit.

---
+++ github-green-box.py
@@ -0,0 +1,51 @@
+import datetime
+import os
+
+from random import randint
+
+def write_log(date):
+  f = open('log.txt', 'a+')
+  f.writelines(date.isoformat() + '\n')
+  f.close()
+
+def commit_github(date, origin):
+  os.system('git add .')
+  os.system('git commit --date={date} -m "Update {date}."'.format(date=date.isoformat()))
+  os.system('git push origin %s' % origin)
+
+def transformation(date) -> datetime.date:
+  return datetime.timedelta(hours=randint(0, 24), minutes=randint(0, 60), seconds=randint(0, 60))
+
+if __name__ == "__main__":
+  print('Please enter you want to commit date (like 20190902): ', end='')
+
+  commit_date = input()
+
+  if commit_date == '' or len(commit_date) != 8:
+    print('\033[31mUnknown date, please enter the correct format !!')
+    exit(1)
+
+  print('Please enter the number of times you want to commit, it must be a number value: ', end='')
+
+  commit_count = int(input())
+
+  if commit_count <= 0:
+    print('\033[31mPlease enter number and it greater than zero !!')
+    exit(1)
+
+  print('Please enter you want push the origin branch: ', end='')
+
+  commit_origin = input()
+
+  if commit_origin == '':
+    print('\033[31mPlease enter you want to push origin !!')
+    exit(1)
+
+  while commit_count > 0:
+    date = datetime.datetime.strptime(commit_date, '%Y%m%d')
+    date += transformation(date)
+
+    write_log(date)
+    commit_github(date.isoformat(), commit_origin)
+
+    commit_count -= 1
